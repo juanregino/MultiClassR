06eb70724d49967a2e0b8b7e87b143d310647302
juanregino <juanreginopenagos@gmail.com>
2024-06-05 23:36:05 +1200

    init config

---
+++ src/main/java/com/multiclassR/MultiClassRApplication.java
@@ -0,0 +1,13 @@
+package com.multiclassR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiClassRApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiClassRApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/multiclassR/MultiClassRApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiclassR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiClassRApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
