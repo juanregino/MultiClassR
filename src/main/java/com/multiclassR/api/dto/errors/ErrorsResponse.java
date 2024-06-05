fca5a2481fcb896dadf861ad438a50347f359551
juanregino <juanreginopenagos@gmail.com>
2024-06-05 08:00:35 -0500

    create errors_handlers

---
+++ src/main/java/com/multiclassR/api/dto/errors/ErrorsResponse.java
@@ -0,0 +1,18 @@
+package com.multiclassR.api.dto.errors;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorsResponse extends BaseErrorResponse {
+  private List<String> errors;
+}
